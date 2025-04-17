20e88e36c353e9d10339fef99172d753eddc36b4
vite234321 <hurtop123412@gmail.com>
2025-04-17 14:05:59 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger, DateTime
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 class Subscription(Base):
-    __tablename__ = "subscriptions"  # Добавлено
+    __tablename__ = "subscriptions"
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(BigInteger, index=True)
     ticker = Column(String, index=True)
+    updated_at = Column(DateTime, default=datetime.utcnow)
